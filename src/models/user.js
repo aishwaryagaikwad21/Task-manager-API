483c4dd40a3fad4454669b1a6726989d04acce1b
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-06-23 16:26:35 +0530

    delete profile

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const validator = require('validator');
 const jwt = require('jsonwebtoken')
+
+const Task = require('./document')
 const userSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -91,11 +93,17 @@ userSchema.pre('save',async function(next){
         user.password = await bcrypt.hash(user.password,8)
     }
     // if(user.isModified('password')){
-        
     //     user.password = await bcrypt.hash(user.password,8)
     // }
+    next()
+})
 
-     next()
+//Delete task when user is removed
+
+userSchema.pre('remove',async function(next){
+    const user = this
+    await Task.deleteMany({owner:user._id})
+    next()
 })
 
 const User = mongoose.model('User',userSchema)
