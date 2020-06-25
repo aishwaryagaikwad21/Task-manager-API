b64a5b3714efc3074d9aef1df4f0f6501e6e2ed2
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-06-25 16:36:17 +0530

    multer upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 require('./db/mongoose')
-//const User = require('./models/user');
-//const Document = require('./models/document');
 
 const userRouter = require('./router/user')
 const documentRoute = require('./router/document')
@@ -9,6 +7,23 @@ const app = express();
 
 
 const port = process.env.PORT || 8000;
+
+ const multer = require('multer');
+ const upload = multer({
+     dest:'images/', //destination to store images
+     onError:function(err,next){
+         console.log('error',err)
+         next(err);
+     }
+ })
+ app.post('/upload',upload.single('uploads'),(req,res)=>{
+     try{
+        res.status(200).send()
+     }catch(e){
+        res.status(404).send(e)
+     }
+ })
+//use form-data in postman
 
 app.use(express.json()); //parse incoming json
 app.use(userRouter);
@@ -18,7 +33,45 @@ app.listen(port,()=>{
     console.log('Server is up on port 8000');
 })
 
-const Task = require('./models/document')
+
+
+
+
+// const jwt = require('jsonwebtoken')
+// const myFunction = async()=>{
+//     const token = jwt.sign({ _id:'abc3' },'thisismycourse',{expiresIn:'7 days'})
+//     //console.log('token is')
+//     //console.log(token)
+//     const data = jwt.verify(token,'thisismycourse') //user is authenticated properly
+    
+//     //console.log(data)
+// }
+// myFunction()
+
+// const data = {
+//     name:'aish'
+// }
+// data.toJSon = function(){
+//     return {}
+//     // console.log(this)
+//     // return this
+// }
+//when res.send() is called express calls JSON.stringify()
+//console.log(JSON.stringify(data))
+
+// const bcrypt = require('bcryptjs');
+// const func = async()=>{
+//  
+//  const hashPassword = await bcrypt.hash(password,8)
+//  console.log(password)
+//  console.log(hashPassword)
+//  const isMatch = await bcrypt.compare('PaVd6213@',hashPassword)
+//  console.log(isMatch)
+// }
+// func()
+
+
+//const Task = require('./models/document')
 //  const findOwnerofTask = async()=>{
 //       const task = await Task.findById('5ef1c4bbc5383f152862870a')
 //       await task.populate('owner').execPopulate()//populate is used to populate data from relation
@@ -36,35 +89,3 @@ const Task = require('./models/document')
 //  }
 //  findTaskOfOwner()
 
-const jwt = require('jsonwebtoken')
-const myFunction = async()=>{
-    const token = jwt.sign({ _id:'abc3' },'thisismycourse',{expiresIn:'7 days'})
-    //console.log('token is')
-    //console.log(token)
-    const data = jwt.verify(token,'thisismycourse') //user is authenticated properly
-    
-    //console.log(data)
-}
-myFunction()
-
-const data = {
-    name:'aish'
-}
-data.toJSon = function(){
-    return {}
-    // console.log(this)
-    // return this
-}
-//when res.send() is called express calls JSON.stringify()
-//console.log(JSON.stringify(data))
-
-// const bcrypt = require('bcryptjs');
-// const func = async()=>{
-//  
-//  const hashPassword = await bcrypt.hash(password,8)
-//  console.log(password)
-//  console.log(hashPassword)
-//  const isMatch = await bcrypt.compare('PaVd6213@',hashPassword)
-//  console.log(isMatch)
-// }
-// func()
